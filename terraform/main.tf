77586dfa53a7d6dad4347f45ffdd8b577b8058c4
Nick Kocharhook <nick@kocharhook.com>
2023-11-27 16:53:51 +0000

    Beginning of Oracle Cloud setup

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+#
+# Based on https://github.com/oracle/terraform-provider-oci/blob/master/examples/always_free/main.tf
+#
+
+terraform {
+  cloud {
+    organization = "nk9"
+
+    workspaces {
+      name = "familypics"
+    }
+  }
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+provider "oci" {
+  region           = "uk-london-1"
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
+
+variable "instance_shape" {
+  default = "VM.Standard.A1.Flex" # Or VM.Standard.E2.1.Micro
+}
+
+variable "instance_ocpus" { default = 1 }
+
+variable "instance_shape_config_memory_in_gbs" { default = 6 }
+
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = 1
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+  default = ""
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+  default = ""
+}
+
+variable "ssh_public_key" {
+  default = ""
+}
+
+variable "compartment_ocid" {
+}
+
+variable "region" {
+}
